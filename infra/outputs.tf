344a8db9f1a78002e64ad6b2305e2afcbd1f7ba8
ntno <ntno@users.noreply.github.com>
2022-12-27 16:48:58 -0500

    output cd role info

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,10 +3,15 @@ output "ci_role_arn" {
   value       = module.demo_site_cicd.ci_role_info.arn
 }
 
-# output "cd_role_arn" {
-#   description = "CD IAM Role ARN"
-#   value       = module.demo_site_ci_cd.cd_role_arn
-# }
+output "cd_role_info" {
+  description = "environment name to Map containing CD IAM Role's arn and associated github_environment_name"
+  value = {
+    for key, val in module.demo_site_cicd.cd_role_info : key => {
+      cd_role_arn             = val.arn
+      github_environment_name = val.github_environment_name
+    }
+  }
+}
 
 output "artifact_bucket_name" {
   description = "artifact S3 bucket name"
